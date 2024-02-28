c5a5edba685d44dbdcc7d64f8488f4c449410f2d
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-02-28 12:14:03 +0100

    Creating basic structure

---
+++ src/MekkdonaldsModel/Simulation/Direction.cs
@@ -0,0 +1,16 @@
+﻿namespace Mekkdonalds.Simulation;
+
+internal enum Direction
+{
+    North,
+    East,
+    South,
+    West
+}
+
+internal static class DirectionMethods
+{
+    internal static Direction ClockWise(this Direction original) => (Direction)(((int)original + 1) % 4);
+
+    internal static Direction CounterClockWise(this Direction original) => (Direction)((3 + (int)original) % 4);
+}
