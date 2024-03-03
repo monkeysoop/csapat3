ad296946dd2df194cc472cbec2c704f5ab0dbb67
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-03 17:34:45 +0100

    Test classes

---
+++ src/MekkdonaldsTest/BotTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Mekkdonalds.Test;
+
+public class BotTests
+{
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
