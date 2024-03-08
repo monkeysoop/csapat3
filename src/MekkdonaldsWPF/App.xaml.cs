aa58e4748be02b211491b06dd326eba8725bc6b9
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-08 22:57:40 +0100

    grid math

--- src/MekkdonaldsWPF/App.xaml.cs
+++ src/MekkdonaldsWPF/App.xaml.cs
@@ -7,6 +7,10 @@ public partial class App : Application
 {
     private const int MARGIN = 20;
     private const int BORDERTHICKNESS = 4;
+
+    private double XLength;
+    private double YLength;
+
     private MainWindow? _mainWindow;
     private SimViewModel? _viewModel;
 
@@ -24,8 +28,10 @@ public partial class App : Application
 
         _mainWindow.SizeChanged += (_, _) => Redraw(_mainWindow.MapCanvas);
 
+        _viewModel = new MainWindowViewModel();
+
         _mainWindow.Show();
-        Draw(_mainWindow.MapCanvas);
+        Redraw(_mainWindow.MapCanvas); // calling redraw to calculate XLength and YLength
     }
 
     /// <summary>
@@ -34,6 +40,23 @@ public partial class App : Application
     /// <param name="c">The currently open window's canvas</param>
     private void Redraw(Canvas c)
     {
+        var (w, h) = _viewModel!.Size;
+
+        if (w > h)
+        {
+            XLength = c.ActualWidth - 2 * MARGIN;
+            YLength = XLength * h / w;
+        }
+        else if (w < h)
+        {
+            YLength = c.ActualHeight - 2 * MARGIN;
+            XLength = YLength * w / h;
+        }
+        else
+        {
+            XLength = YLength = Math.Min(c.ActualHeight, c.ActualWidth) - 2 * MARGIN;
+        }
+
         c.Children.Clear();
         Draw(c);
     }
@@ -45,71 +68,112 @@ public partial class App : Application
     private void Draw(Canvas c)
     {
         DrawFrame(c);
+        DrawGrid(c);
     }
 
     /// <summary>
     /// Draws the frame of the map
     /// </summary>
     /// <param name="c">The currently open window's canvas</param>
-    private static void DrawFrame(Canvas c)
+    private void DrawFrame(Canvas c)
     {
-        var length = Math.Min(c.ActualHeight, c.ActualWidth) - MARGIN;
-
         List<Line> l = [];
 
+        // TOP
         l.Add(
-            new Line() // top
+            new Line()
             {
                 Stroke = Brushes.Black,
                 StrokeThickness = BORDERTHICKNESS,
                 X1 = MARGIN - 2,
                 Y1 = MARGIN,
-                X2 = length + 2,
+                X2 = XLength + 2,
                 Y2 = MARGIN,
             }
         );
 
+        // BOTTOM
         l.Add(
-            new Line() // bottom
+            new Line()
             {
                 Stroke = Brushes.Black,
                 StrokeThickness = BORDERTHICKNESS,
                 X1 = MARGIN - 2,
-                Y1 = length,
-                X2 = length + 2,
-                Y2 = length,
+                Y1 = YLength,
+                X2 = XLength + 2,
+                Y2 = YLength,
             }
          );
 
+        // LEFT
         l.Add(
-            new Line() // left
+            new Line()
             {
                 Stroke = Brushes.Black,
                 StrokeThickness = BORDERTHICKNESS,
                 X1 = MARGIN,
                 Y1 = MARGIN,
                 X2 = MARGIN,
-                Y2 = length,
+                Y2 = YLength,
             }
         );
 
+        // RIGHT
         l.Add(
-            new Line() // right
+            new Line()
             {
                 Stroke = Brushes.Black,
                 StrokeThickness = BORDERTHICKNESS,
-                X1 = length,
+                X1 = XLength,
                 Y1 = MARGIN,
-                X2 = length,
-                Y2 = length,
+                X2 = XLength,
+                Y2 = YLength,
             }
         );
 
         l.ForEach(x => c.Children.Add(x));
     }
 
-    private static void DrawRobots(List<Robot> r)
-    {
 
+    private void DrawGrid(Canvas c)
+    {
+        var xStep = (XLength - MARGIN) / _viewModel!.Size.W;
+        var ystep = (YLength - MARGIN) / _viewModel!.Size.H;
+
+        for (var i = 1; i < _viewModel!.Size.W; i++)
+        {
+            c.Children.Add(
+                new Line()
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1,
+                    X1 = MARGIN + i * xStep,
+                    Y1 = MARGIN,
+                    X2 = MARGIN + i * xStep,
+                    Y2 = YLength,
+                }
+            );
+        }
+
+        for (var i = 1; i < _viewModel!.Size.H; i++)
+        {
+            c.Children.Add(
+                new Line()
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1,
+                    X1 = MARGIN,
+                    Y1 = MARGIN + i * ystep,
+                    X2 = XLength,
+                    Y2 = MARGIN + i * ystep,
+                }
+            );
+        }
+    }
+
+    private void DrawRobots()
+    {
+        _ = _viewModel!.Robots[0];
+        throw new NotImplementedException();
     }
 }
