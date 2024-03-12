0f9be43f42969d2c99484769f31b578b0b2079f4
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-12 21:52:48 +0100

    Added replay controller class

---
+++ src/MekkdonaldsModel/Simulation/Controller/ReplayController.cs
@@ -0,0 +1,9 @@
+﻿namespace Mekkdonalds.Simulation.Controller;
+
+public sealed class ReplayController : Controller
+{
+    protected override void OnTick(object? state)
+    {
+        throw new NotImplementedException();
+    }
+}
