fea63499c9188b35c99a320e9faf9742a818ceb9
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-12 21:53:09 +0100

    created replay view model

---
+++ src/MekkdonaldsWPF/ViewModel/ReplayViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Mekkdonalds.Simulation.Controller;
+
+namespace Mekkdonalds.ViewModel;
+
+internal class ReplayViewModel : ViewModel
+{
+    private readonly ReplayController Controller;
+
+    public ReplayViewModel()
+    {
+        Size = (20, 40);
+
+        Controller = new ReplayController();
+
+        _walls.AddRange(Controller.Walls);
+        _robots.AddRange(Controller.Robots);
+
+        Controller.Tick += (_, _) => OnTick(this);
+    }
+}
