50286790001309e6f4cbc57ba3ec74afb78b9120
Istók Endre <lv4111@inf.elte.hu>
2024-03-12 15:12:43 +0100

    adding classes for board data access

---
+++ src/MekkdonaldsModel/Persistence/IBoardDataAccess.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace MekkdonaldsModel.Persistence
+{
+    internal interface IBoardDataAccess
+    {
+        internal Task<Board2> LoadAsync(string path);
+        internal Task SaveAsync(string path, Board2 board);
+    }
+}
