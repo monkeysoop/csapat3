e9683afb7447b5bafcff8a3283b928d83eee27d4
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-12 21:52:58 +0100

    Created replay window

---
+++ src/MekkdonaldsWPF/View/ReplayWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Mekkdonalds.View;
+
+/// <summary>
+/// Interaction logic for ReplayWindow.xaml
+/// </summary>
+public partial class ReplayWindow : Window
+{
+    public ReplayWindow()
+    {
+        InitializeComponent();
+    }
+}
