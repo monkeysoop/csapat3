bfd53b0b6de2d3547d2a1247a8b1d91ba42e91f7
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-12 21:53:22 +0100

    created start window

---
+++ src/MekkdonaldsWPF/View/StartWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Mekkdonalds.View;
+
+/// <summary>
+/// Interaction logic for StartWindow.xaml
+/// </summary>
+public partial class StartWindow : Window
+{
+    public StartWindow()
+    {
+        InitializeComponent();
+    }
+}
