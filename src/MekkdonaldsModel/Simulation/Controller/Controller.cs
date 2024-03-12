58e5540a0010c66c245abac70e6fe5a64744307e
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-12 20:42:18 +0100

    Added Board2 field

--- src/MekkdonaldsModel/Simulation/Controller/Controller.cs
+++ src/MekkdonaldsModel/Simulation/Controller/Controller.cs
@@ -7,6 +7,7 @@ public abstract class Controller
     protected List<Wall> _walls;
     protected Timer Timer;
 
+    protected Board2 _board;
 
     public IReadOnlyList<Robot> Robots => _robots.AsReadOnly();
 
@@ -19,6 +20,8 @@ public abstract class Controller
         Paths = [];
         _robots = [];
         _walls = [];
+
+        _board = new(0, 0);
 
         Timer = new Timer(OnTick, null, Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan); // this is probably better then System.Timers.Timer (it is already asynchronous)
     }
