e674f126527cd7dccbf95b19832520da6753c5e9
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-18 00:30:53 +0100

    Reverted commit that remove the indexing property from Board2

--- src/MekkdonaldsModel/Persistence/Board2.cs
+++ src/MekkdonaldsModel/Persistence/Board2.cs
@@ -5,18 +5,40 @@ namespace MekkdonaldsModel.Persistence;
 // Has to be public otherwise can't be used in Controller class
 public class Board2
 {
+    #region Constants
     public const int EMPTY = 0;
     public const int WALL = 1;
     public const int SEARCHED = 2;
     public const int OPEN = 3;
+    #endregion
 
+
+
+    #region Fields
     private readonly int[] Data;
-
     public int Height { get; init; }
     public int Width { get; init; }
+    #endregion
 
 
 
+    #region Properties
+    public int this[int y, int x]
+    {
+        get { return Data[y * Width + x]; }
+        set { Data[y * Width + x] = value; }
+    }
+
+    public int this[Point p]
+    {
+        get { return Data[p.Y * Width + p.X]; }
+        set { Data[p.Y * Width + p.X] = value; }
+    }
+    #endregion
+
+
+
+    #region Constructors
     public Board2(int height, int width)
     {
         Data = new int[height * width];
@@ -38,18 +60,22 @@ public class Board2
     public Board2(int[,] data, int height, int width)
     {
         Data = new int[height * width];
-        for (int y = 0; y < height; y++) 
+        for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                Data[y * width + x] = data[y, x]; 
+                this[y, x] = data[y, x];
             }
         }
 
         Height = height;
         Width = width;
     }
+    #endregion
 
+
+
+    #region Public methods
     public void SetSearched(Point position)
     {
 #if NO_BORDER_CHECK
@@ -62,24 +88,24 @@ public class Board2
 #endif
         if (t)
         {
-            Data[position.Y * Width + position.X] = SEARCHED;
+            this[position] = SEARCHED;
         }
     }
 
     public bool SetOpenIfEmpty(Point position)
     {
 #if NO_BORDER_CHECK
-        bool t = data[position.Y * width + position.X] == EMPTY;
+        bool t = this[position] == EMPTY;
 #else
         bool t = position.X >= 0 &&
                  position.X < Width &&
                  position.Y >= 0 &&
                  position.Y < Height &&
-                 Data[position.Y * Width + position.X] == EMPTY;
+                 this[position] == EMPTY;
 #endif
         if (t)
         {
-            Data[position.Y * Width + position.X] = OPEN;
+            this[position] = OPEN;
         }
 
         return t;
@@ -88,17 +114,17 @@ public class Board2
     public bool SetSearchedIfEmpty(Point position)
     {
 #if NO_BORDER_CHECK
-        bool t = data[position.Y * width + position.X] == EMPTY;
+        bool t = this[position] == EMPTY;
 #else
         bool t = position.X >= 0 &&
                  position.X < Width &&
                  position.Y >= 0 &&
                  position.Y < Height &&
-                 Data[position.Y * Width + position.X] == EMPTY;
+                 this[position] == EMPTY;
 #endif
         if (t)
         {
-            Data[position.Y * Width + position.X] = SEARCHED;
+            this[position] = SEARCHED;
         }
 
         return t;
@@ -163,4 +189,8 @@ public class Board2
             throw new Exception("Map loading error");
         }
     }
+
+#endregion
 }
+
+
---
+++ src/MekkdonaldsModel/Persistence/BoardFileDataAccess.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace MekkdonaldsModel.Persistence
+{
+    internal class BoardFileDataAccess : IBoardDataAccess
+    {
+        public async Task<Board2> LoadAsync(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SaveAsync(string path, Board2 board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
