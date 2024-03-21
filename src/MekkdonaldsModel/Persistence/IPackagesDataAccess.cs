a966275d52471c840b7ab745271fcec30dcc0c1d
Istók Endre <lv4111@inf.elte.hu>
2024-03-21 11:22:42 +0100

    adding interface for robot and pacgkage files and modell.cs class

---
+++ src/MekkdonaldsModel/Persistence/IPackagesDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using Mekkdonalds.Simulation;
+
+namespace MekkdonaldsModel.Persistence
+{
+    internal interface IPackagesDataAccess
+    {
+        internal Task<List<Package>> LoadAsync(string path);
+        internal Task SaveAsync(string path, List<Package> packages);
+    }
+}
