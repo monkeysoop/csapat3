2544a8f1af67d82d9e0c38a438da5304af1bda8f
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-03-23 23:22:27 +0100

    Created Converter for Events

---
+++ src/MekkdonaldsModel/Persistence/EventConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MekkdonaldsModel.Persistence
+{
+    internal class EventConverter : JsonConverter<(int, int, string)>
+    {
+        public override (int, int, string) Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString() ?? throw new NullReferenceException();
+            value = value.Replace("\n", "");
+            value = value.Replace("[", "");
+            value = value.Replace("]", "");
+            var values = value.Split(",");
+            return (int.Parse(values[0]), int.Parse(values[1]), values[2]);
+        }
+
+        public override void Write(Utf8JsonWriter writer, (int, int, string) value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue($"[{value.Item1},{value.Item1},{value.Item3}");
+
+        }
+    }
+}
