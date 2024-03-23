2c106631b4fb3eb4d302bff5e65bb9a150de6363
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-03-23 23:22:37 +0100

    Updated Direction Methods

--- src/MekkdonaldsModel/Simulation/Direction.cs
+++ src/MekkdonaldsModel/Simulation/Direction.cs
@@ -1,4 +1,6 @@
-﻿namespace Mekkdonalds.Simulation;
+﻿using static System.Collections.Specialized.BitVector32;
+
+namespace Mekkdonalds.Simulation;
 
 public enum Direction
 {
@@ -13,4 +15,40 @@ public static class DirectionMethods
     public static Direction ClockWise(this Direction original) => (Direction)(((int)original + 1) % 4);
 
     public static Direction CounterClockWise(this Direction original) => (Direction)((3 + (int)original) % 4);
+
+    public static string DirectionToString(this Direction original)
+    {
+        switch (original)
+        {
+            case Direction.North:
+                return "N";
+            case Direction.East:
+                return "E";
+            case Direction.South:
+                return "S";
+            case Direction.West:
+                return "W";
+            default:
+                return original.ToString();
+        }
+    }
+
+    public static Direction StringToDirection(string original)
+    {
+        switch (original)
+        {
+            case "N":
+                return Direction.North;
+            case "E":
+                return Direction.East;
+            case "S":
+                return Direction.South;
+            case "W":
+                return Direction.West;
+            default:
+                throw new ArgumentException();
+        }
+    }
+
+
 }
