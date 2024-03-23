23ab85226361946f1210749b0668b84ca69e655d
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-03-23 22:11:52 +0100

    Created Exception for LogFileDataAccess

---
+++ src/MekkdonaldsModel/Exception/LogFileDataException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MekkdonaldsModel.Exception
+{
+    internal class LogFileDataException : System.Exception
+    {
+        public LogFileDataException()
+        {
+        }
+
+        public LogFileDataException(string? message) : base(message)
+        {
+        }
+
+        public LogFileDataException(string? message, System.Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
