09ff982d7ff852dcf832d4b8d36dba9dceec0f98
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-03-23 23:23:45 +0100

    Created the Structure of Logfile

--- src/MekkdonaldsModel/Persistence/LogFile.cs
+++ src/MekkdonaldsModel/Persistence/LogFile.cs
@@ -1,19 +1,34 @@
-﻿using System.Text.Json.Serialization;
+﻿using MekkdonaldsModel.Persistence;
+using System.Text.Json.Serialization;
 
 namespace Mekkdonalds.Persistence;
 
 internal class LogFile
 {
-    internal static LogFile Load()
-    {
-        throw new NotImplementedException();
-    }
+    public required string ActionModel { get; set; }
 
-    internal void Save()
-    {
-        throw new NotImplementedException();
-    }
+    [JsonConverter(typeof(YesNoConverter))]
+    public bool AllValid { get; set; }
+    public int TeamSize { get; set; }
+    [JsonConverter(typeof(StartPosConverter))]
+    public required List<(Point, Direction)> Start { get; set; }
+
+    public int NumTaskFinished { get; set; }
+    public int SumOfCost { get; set; }
+    public int Makespan { get; set; }
+
+    public required List<string> ActualPaths { get; set; }
+    public required List<string> PlannerPaths { get; set; }
+    public required List<double> PlannerTimes { get; set; }
+    [JsonConverter(typeof(ErrorConverter))]
+    public required List<(int, int, int, string)> Errors { get; set; }
+    [JsonConverter(typeof(EventConverter))]
+    public required List<List<(int, int, string)>> Events { get; set; }
+    [JsonConverter(typeof(TaskConverter))]
+    public required List<(int, int, int)> Tasks { get; set; }
+
+
 
     [JsonConstructor]
-    public Config() { }
+    public LogFile() { }
 }
