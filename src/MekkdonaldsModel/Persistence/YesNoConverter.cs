efb4ed7f7798078b2214e35d1759d1bbd6f47a30
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-03-23 23:21:42 +0100

    Created Converter for AllValid

---
+++ src/MekkdonaldsModel/Persistence/YesNoConverter.cs
@@ -0,0 +1,34 @@
+﻿using Mekkdonalds.Simulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MekkdonaldsModel.Persistence
+{
+    public class YesNoConverter : JsonConverter<bool>
+    {
+        public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var yesorno = reader.GetString();
+
+            return yesorno switch
+            {
+                "Yes" => true,
+                "No" => false,
+                _ => throw new JsonException()
+            };
+        }
+
+        public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value ? "Yes" : "No");
+
+
+        }
+    }
+}
