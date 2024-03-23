3c4f8842c35d9b7f0bf7fb8a048a7a0f57d084a2
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-03-23 23:21:27 +0100

    Created Converter for Errors

---
+++ src/MekkdonaldsModel/Persistence/ErrorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MekkdonaldsModel.Persistence
+{
+    internal class ErrorConverter : JsonConverter<(int, int, int, string)>
+    {
+        public override (int, int, int, string) Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString() ?? throw new NullReferenceException();
+            value = value.Replace("\n", "");
+            value = value.Replace("[", "");
+            value = value.Replace("]", "");
+            var values = value.Split(",");
+            return (int.Parse(values[0]), int.Parse(values[1]), int.Parse(values[2]), values[3]);
+        }
+
+        public override void Write(Utf8JsonWriter writer, (int, int, int, string) value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue($"[{value.Item1},{value.Item1},{value.Item3},{value.Item4}");
+
+        }
+    }
+}
