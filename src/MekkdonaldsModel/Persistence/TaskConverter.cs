ea162b7cd807511043e7ace5c753ec2e27e19b52
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-03-23 23:21:58 +0100

    Created Converter for Tasks

---
+++ src/MekkdonaldsModel/Persistence/TaskConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MekkdonaldsModel.Persistence
+{
+    internal class TaskConverter : JsonConverter<(int, int, int)>
+    {
+        public override (int, int, int) Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString() ?? throw new NullReferenceException();
+            value = value.Replace("\n", "");
+            value = value.Replace("[", "");
+            value = value.Replace("]", "");
+            var values = value.Split(",");
+            return (int.Parse(values[0]), int.Parse(values[1]), int.Parse(values[2]));
+        }
+
+        public override void Write(Utf8JsonWriter writer, (int, int, int) value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue($"[{value.Item1},{value.Item1},{value.Item3}");
+        }
+    }
+}
