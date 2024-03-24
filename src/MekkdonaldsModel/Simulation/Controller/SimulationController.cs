e2968f4c4c9f5bd2a9312f1ab1c2ed3dbbd7e1a0
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-24 01:09:11 +0100

    Added SimulationController class

--- src/MekkdonaldsModel/Simulation/Controller/SimulationController.cs
+++ src/MekkdonaldsModel/Simulation/Controller/SimulationController.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mekkdonalds.Simulation.Controller;
 
-public abstract class SimulationController
+public sealed class SimulationController : Controller
 {
     protected static readonly Point[] nexts_offsets = [
         new(0, -1),
@@ -64,77 +64,9 @@ public abstract class SimulationController
             }
         }
 
-        return l;
-    }
-    
-    protected static bool ComparePoints(Point first, Point second) // == is overloaded
-    {
-        return first.X == second.X && first.Y == second.Y;
-    }
-
-    protected static int ManhattenDistance(Point start, Point end)
-    {
-        return Math.Abs(start.X - end.X) + Math.Abs(start.Y - end.Y);
-    }
-
-    protected static int MaxTurnsRequired(Point position, Point direction, Point end)
-    {
-        int diff_x = end.X - position.X;
-        int diff_y = end.Y - position.Y;
-        int dot_product = diff_x * direction.X + diff_y * direction.Y;
-
-        if (dot_product < 0)
-        {
-            return 2;
-        }
-        else if (dot_product * dot_product != diff_x * diff_x + diff_y * diff_y)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-
-    private static List<Action> TracePath(int[] parents_board, int board_width, Point start, int start_direction, Point end)
-    {
-        List<Action> path = new List<Action>();
-
-
-        Point current_position = end;
-        int current_direction = (parents_board[current_position.Y * board_width + current_position.X] + 2) % 4;
-
-        while (!ComparePoints(current_position, start))
-        {
-            Point next_offset = nexts_offsets[current_direction];
-
-            Point next_position = new(current_position.X + next_offset.X,
-                                      current_position.Y + next_offset.Y);
-
-            int next_direction = (parents_board[next_position.Y * board_width + next_position.X] + 2) % 4;
-
-            int diff = current_direction - next_direction + 3;
-
-
-            path.Add(Action.F);
-            switch (diff)
+    protected override void OnTick(object? state)
             {
-                case -3: path.Add(Action.R); break;
-                case -2: path.Add(Action.R); path.Add(Action.R); break;
-                case -1: path.Add(Action.C); break;
-                case  0: break;
-                case  1: path.Add(Action.R); break;
-                case  2: path.Add(Action.R); path.Add(Action.R); break;
-                case  3: path.Add(Action.C); break;
-            }
-
-            current_position = next_position;
-            current_direction = next_direction;
-        }
-
-        System.Diagnostics.Debug.WriteLine("path in reverse: " + path);
-        return path;
+        Debug.WriteLine("Tick");
     }
 
 }
