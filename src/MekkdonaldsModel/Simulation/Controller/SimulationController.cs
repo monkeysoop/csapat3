243b5f94fe61039ebeed5d1b4a1db96731a9e066
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-24 01:09:30 +0100

    Implemented loading for simulation controller

--- src/MekkdonaldsModel/Simulation/Controller/SimulationController.cs
+++ src/MekkdonaldsModel/Simulation/Controller/SimulationController.cs
@@ -2,71 +2,34 @@
 
 public sealed class SimulationController : Controller
 {
-    protected static readonly Point[] nexts_offsets = [
-        new(0, -1),
-        new(1, 0),
-        new(0, 1),
-        new(-1, 0)
-    ];
-
-    protected abstract (bool, int[]) FindPath(Board2 board, Point start_position, int start_direction, Point end_position);
-
-    public (bool, List<Action>) CalculatePath(Board2 board, Point start_position, int start_direction, Point end_position)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="path">Path of the config file</param>
+    /// <param name="ca"></param>
+    /// <param name="ba"></param>
+    /// <param name="ra"></param>
+    /// <param name="pa"></param>
+    public SimulationController(string path, IConfigDataAccess ca, IBoardDataAccess ba, IRobotsDataAccess ra, IPackagesDataAccess pa)
     {
-        bool found;
-        int[] parents_data;
-
-        (found, parents_data) = FindPath(board, start_position, start_direction, end_position);
-        board.ClearMask();
-
-        if (found)
-        {
-            return (true, TracePath(parents_data, board.Width, start_position, start_direction, end_position));
-        } else
-        {
-            return (false, new List<Action>());
-        }
+        Load(path, ca, ba, ra, pa);
     }
 
-    public void FindAllPaths(Board2 board, List<Robot> robots, List<Package> packages)
+    private async void Load(string path, IConfigDataAccess da, IBoardDataAccess ba, IRobotsDataAccess ra, IPackagesDataAccess pa)
     {
-        while (packages.Count > 0)
-        {
-            foreach(Robot r  in robots)
-            {
-                
-            }
-        }
+        var config = await da.Load(path);
+
+        var b = await ba.LoadAsync(config.MapFile);
+        _board = b; // for some reason it only sets board this way ????????
+
+        _robots.AddRange(await ra.LoadAsync(config.AgentFile, _board.Width, _board.Height));
+        _packages.AddRange(await pa.LoadAsync(config.TaskFile, _board.Width, _board.Height));
+
+        OnLoaded(this);
     }
-
-    public static List<Action> Convert(string path)
-    {
-        var l = new List<Action>();
-
-        foreach (char c in path)
-        {
-            switch (c)
-            {
-                case 'F':
-                    l.Add(Action.F);
-                    break;
-                case 'R':
-                    l.Add(Action.R);
-                    break;
-                case 'L':
-                    l.Add(Action.C);
-                    break;
-                case 'W':
-                    l.Add(Action.W);
-                    break;
-                default: // can be removed
-                    throw new PathException($"Invalid charachter {c}");
-            }
-        }
 
     protected override void OnTick(object? state)
-            {
+    {
         Debug.WriteLine("Tick");
     }
-
 }
