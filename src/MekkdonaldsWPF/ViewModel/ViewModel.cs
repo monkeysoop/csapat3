e33bba6a7da3bbae87481d40f6dbfc3ffcf7b628
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-24 01:11:21 +0100

    Removed unnecessary properties and fields and added abstract Controller object

--- src/MekkdonaldsWPF/ViewModel/ViewModel.cs
+++ src/MekkdonaldsWPF/ViewModel/ViewModel.cs
@@ -5,29 +5,16 @@
 /// </summary>
 internal abstract class ViewModel : ViewModelBase
 {
-    protected List<Robot> _robots = [];
-    protected List<Wall> _walls = [];
+#pragma warning disable CS8618 // :)
+    protected Controller Controller;
+#pragma warning restore CS8618
 
-    private (int W, int H) _size;
     private double _zoom = 1;
 
     #region Properties
 
-    /// <summary>
-    /// Size of the grid (Collumns, Rows)
-    /// </summary>
-    public (int W, int H) Size
-    {
-        get => _size;
-        protected set
-        {
-            if (_size != value)
-            {
-                _size = value;
-                OnPropertyChanged(nameof(Size));
-            }
-        }
-    }
+    public int Width => Controller.Width;
+    public int Height => Controller.Height;
 
     public double Zoom
     {
@@ -48,11 +35,11 @@ internal abstract class ViewModel : ViewModelBase
     /// <summary>
     /// Robots present on the grid
     /// </summary>
-    public IReadOnlyList<Robot> Robots => _robots.AsReadOnly();
+    public IReadOnlyList<Robot> Robots => Controller.Robots;
     /// <summary>
     /// Walls present on the grid
     /// </summary>
-    public IReadOnlyList<Wall> Walls => _walls.AsReadOnly();
+    public IReadOnlyList<Wall> Walls => Controller.Walls;
 
     #endregion
 
@@ -60,6 +47,13 @@ internal abstract class ViewModel : ViewModelBase
     /// Eventhandler thats called each time the grid gets updated
     /// </summary>
     public event EventHandler? Tick;
+    public event EventHandler? Loaded;
+
+    protected void OnLoaded(object? sender)
+    {
+        Loaded?.Invoke(sender, EventArgs.Empty);
+    }
+
     /// <summary>
     /// Calls the tick event based on the models evnt handler
     /// </summary>
