c436ee433deb6d4825516f45f78b8e7031d04ff5
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-03-27 15:03:47 +0100

    Added abstract pathfinding

---
+++ src/MekkdonaldsModel/Simulation/PathFinding/IPathFinder.cs
@@ -0,0 +1,8 @@
+﻿namespace Mekkdonalds.Simulation.PathFinding;
+
+public interface IPathFinder
+{
+    void Init(Board2 board, IEnumerable<Package> packages);
+    void Assign(Robot r);
+    void Step(Robot r);
+}
