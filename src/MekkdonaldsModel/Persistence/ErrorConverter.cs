ed66aa003498ad0b57308d4b9bd91a18d7e3ebc7
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-04-01 23:25:15 +0200

    Made converter for errors

--- src/MekkdonaldsModel/Persistence/ErrorConverter.cs
+++ src/MekkdonaldsModel/Persistence/ErrorConverter.cs
@@ -3,21 +3,33 @@ using System.Text.Json.Serialization;
 
 namespace MekkdonaldsModel.Persistence
 {
-    internal class ErrorConverter : JsonConverter<(int, int, int, string)>
+    internal class ErrorConverter : JsonConverter<List<(int, int, int, string)>>
     {
-        public override (int, int, int, string) Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override List<(int, int, int, string)> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             var value = reader.GetString() ?? throw new NullReferenceException();
             value = value.Replace("\n", "");
+            value = value.Replace("\t", "");
+            value = value.Replace(" ", "");
             value = value.Replace("[", "");
             value = value.Replace("]", "");
             var values = value.Split(",");
-            return (int.Parse(values[0]), int.Parse(values[1]), int.Parse(values[2]), values[3]);
+            var lista = new List<(int, int, int, string)>();
+            for (int i = 0; i < values.Length; i += 4)
+            {
+                var tuple = (int.Parse(values[i]), int.Parse(values[i + 1]), int.Parse(values[i + 2]), values[i + 3]);
+                lista.Add(tuple);
+            }
+            return lista;
         }
 
-        public override void Write(Utf8JsonWriter writer, (int, int, int, string) value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter writer, List<(int, int, int, string)> value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue($"[{value.Item1},{value.Item1},{value.Item3},{value.Item4}");
+            for (int i = 0; i < value.Count; i++)
+            {
+                writer.WriteStringValue($"[{value[i].Item1},{value[i].Item2},{value[i].Item3},{value[i].Item4}]");
+
+            }
 
         }
     }
