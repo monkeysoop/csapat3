4500db2f2be4afc6216e48234488c32b048324c7
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-04-01 23:25:26 +0200

    Made converter for events

--- src/MekkdonaldsModel/Persistence/EventConverter.cs
+++ src/MekkdonaldsModel/Persistence/EventConverter.cs
@@ -11,16 +11,45 @@ namespace MekkdonaldsModel.Persistence
             value = value.Replace("\n", "");
             value = value.Replace("\t", "");
             value = value.Replace(" ", "");
+            value = value.Replace("[[", "lb");
+            value = value.Replace("]]", "le");
             value = value.Replace("[", "");
             value = value.Replace("]", "");
-            var values = value.Split(",");
+            var values = value.Split("lb");
+            List<string> stringl = new();
+            foreach (var item in values)
+            {
+                if (item != "") { stringl.Add(item); }
+            }
             List<List<(int, int, string)>> lista = new();
+            List<(int, int, string)> kislista = new();
+            foreach (var item in stringl)
+            {
+                values = item.Split(',');
+                kislista.Clear();
+                for (int i = 0; i < values.Length; i += 3)
+                {
+                    var tuple = (int.Parse(values[i]), int.Parse(values[i + 1]), values[i + 1]);
+                    kislista.Add(tuple);
+                }
+                lista.Add(kislista);
+
+            }
             return lista;
         }
 
         public override void Write(Utf8JsonWriter writer, List<List<(int, int, string)>> value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue($"[{value}");
+            foreach (var item in value)
+            {
+                writer.WriteStringValue($"[");
+                foreach (var item2 in item)
+                {
+                    writer.WriteStringValue($"[{item2.Item1},{item2.Item2},{item2.Item3}]");
+                }
+                writer.WriteStringValue($"]");
+            }
+
 
         }
     }
