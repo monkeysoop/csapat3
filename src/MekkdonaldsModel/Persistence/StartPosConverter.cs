6460998d943e70db8483aa450458cd919c8b2ade
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-04-02 00:39:32 +0200

    Rewrote as example

--- src/MekkdonaldsModel/Persistence/StartPosConverter.cs
+++ src/MekkdonaldsModel/Persistence/StartPosConverter.cs
@@ -7,28 +7,76 @@ namespace MekkdonaldsModel.Persistence
     {
         public override List<(Point, Direction)> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            var value = reader.GetString() ?? throw new NullReferenceException();
-            value = value.Replace("\n", "");
-            value = value.Replace("\t", "");
-            value = value.Replace(" ", "");
-            value = value.Replace("[", "");
-            value = value.Replace("]", "");
-            var values = value.Split(",");
-            var lista = new List<(Point, Direction)>();
-            for (int i = 0; i < values.Length; i += 3)
+            if (reader.TokenType != JsonTokenType.StartArray)
             {
-                var tuple = (new Point(int.Parse(values[i]), int.Parse(values[i + 1])), DirectionMethods.StringToDirection(values[i + 2]));
-                lista.Add(tuple);
+                throw new JsonException();
             }
-            return lista;
+
+            var list = new List<(Point, Direction)>();
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndArray)
+                {
+                    return list;
+                }
+
+                list.Add(ReadValue(ref reader));
+            }
+
+            throw new JsonException();
         }
 
         public override void Write(Utf8JsonWriter writer, List<(Point, Direction)> value, JsonSerializerOptions options)
         {
-            for (int i = 0; i < value.Count; i++)
+            writer.WriteStartArray();
+            foreach (var (p, d) in value)
             {
-                writer.WriteStringValue($"[{value[i].Item1.X},{value[i].Item1.Y},{DirectionMethods.DirectionToString(value[i].Item2)}]");
+                writer.WriteStartArray();
+                writer.WriteNumberValue(p.X);
+                writer.WriteNumberValue(p.Y);
+                writer.WriteStringValue(d.ToString()[0..1]);
+                writer.WriteEndArray();
             }
+            writer.WriteEndArray();
+        }
+
+        private static (Point, Direction) ReadValue(ref Utf8JsonReader reader)
+        {
+            var p = new Point();
+            var d = Direction.North;
+            int i = 0;
+
+            if (reader.TokenType != JsonTokenType.StartArray)
+            {
+                throw new JsonException();
+            }
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndArray)
+                {
+                    return (p, d);
+                }
+
+                if (reader.TokenType == JsonTokenType.Number)
+                {
+                    if (i++ == 0)
+                    {
+                        p.X = reader.GetInt32();
+                    }
+                    else
+                    {
+                        p.Y = reader.GetInt32();
+                    }
+                }                
+                else if (reader.TokenType == JsonTokenType.String)
+                {
+                    d = DirectionMethods.StringToDirection(reader.GetString() ?? throw new JsonException());
+                }
+            }
+
+            throw new JsonException();
         }
     }
 }
