bd77debb3da50c98a3200d053e6de732e3187aa5
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-04-07 22:06:01 +0200

    Created custom interval tree

---
+++ src/MekkdonaldsModel/IntervalTree.cs
@@ -0,0 +1,23 @@
+﻿using IntervalTree;
+
+namespace Mekkdonalds;
+
+public class IntervalTree<TValue> : IntervalTree<int, TValue>
+{
+    public new void Add(int from, int to, TValue value)
+    {
+        base.Add(from, to - 1, value);
+    }
+
+    public new TValue Query(int key) => Query(key, key).FirstOrDefault()!;
+
+    public TValue this[int key] => Query(key);
+
+    public TValue this[int from, int to]
+    {
+        set
+        {
+            base.Add(from, to - 1, value);
+        }
+    }
+}
