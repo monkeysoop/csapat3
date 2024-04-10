e8c0f4abaaa7364e89d5bf6b1f914d19d760bb45
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-04-10 23:49:22 +0200

    Added write save tests

--- src/MekkdonaldsTest/PersistenceTests.cs
+++ src/MekkdonaldsTest/PersistenceTests.cs
@@ -1,6 +1,9 @@
 ﻿using Mekkdonalds.Persistence;
 using Mekkdonalds.Simulation;
+
 using System.Drawing;
+
+using Action = Mekkdonalds.Simulation.Action;
 
 namespace Mekkdonalds.Test;
 
@@ -12,31 +15,73 @@ public class PersistenceTests
     }
 
     [Test]
-    public async Task Test1()
+    public void LogReadTest()
     {
-        LogFileDataAccess logFileDataAccess = new();
-        LogFile l = await logFileDataAccess.Load("../../../../MekkdonaldsWPF/samples/random_20_log.json");
-        //Assert.IsTrue(l.AllValid);
-        Assert.IsFalse(l.AllValid);
-        /*It can't find AllValid in Logfile, because it searches for allValid instant of AllValid, 
-         and for some reason it's already written in camelcase in logfile :)
-         */
-        Assert.That(l.ActionModel, Is.EqualTo("MAPF_T"));
-        Assert.That(l.TeamSize, Is.EqualTo(20));
-        Assert.That(l.Start[0], Is.EqualTo((new Point(4, 6), Direction.East)));
-        Assert.That(l.NumTaskFinished, Is.EqualTo(338));
-        Assert.That(l.SumOfCost, Is.EqualTo(10000));
-        Assert.That(l.Makespan, Is.EqualTo(500));
-        Assert.That(l.ActualPaths[0][0], Is.EqualTo("F"));
-        Assert.That(l.PlannerPaths[0][0], Is.EqualTo("F"));
-        Assert.That(l.PlannerTimes[0], Is.EqualTo(0.312150264));
-        Assert.That(l.Errors, Is.Empty);
-        Assert.That(l.Events, Is.TypeOf<List<List<(int, int, string)>>>());
-        Assert.That(l.Events[0][0], Is.EqualTo((0, 0, "assigned")));
-        Assert.That(l.Tasks[0], Is.EqualTo((0, 21, 6)));
-        l = await logFileDataAccess.Load("../../../../MekkdonaldsWPF/samples/warehouse_100_log.json");
-        Assert.That(l.Errors[0], Is.EqualTo((-1, -1, 1, "incorrect vector size")));
+        Assert.Multiple(async () =>
+        {
+            //Assert.IsTrue(l.AllValid); // It can't find AllValid in Logfile, because it searches for allValid instant of AllValid, and for some reason it's already written in camelcase in logfile :)
 
+            var logFileDataAccess = new LogFileDataAccess();
+            var log = await logFileDataAccess.Load("../../../../MekkdonaldsWPF/samples/random_20_log.json");
 
+            Assert.That(log.ActionModel, Is.EqualTo("MAPF_T"));
+            Assert.That(log.TeamSize, Is.EqualTo(20));
+            Assert.That(log.Start[0], Is.EqualTo((new Point(4, 6), Direction.East)));
+            Assert.That(log.NumTaskFinished, Is.EqualTo(338));
+            Assert.That(log.SumOfCost, Is.EqualTo(10000));
+            Assert.That(log.Makespan, Is.EqualTo(500));
+            Assert.That(log.ActualPaths[0][0], Is.EqualTo(Action.F));
+            Assert.That(log.PlannerPaths[0][0], Is.EqualTo(Action.F));
+            Assert.That(log.PlannerTimes[0], Is.EqualTo(0.312150264));
+            Assert.That(log.Errors, Is.Empty);
+            Assert.That(log.Events, Is.TypeOf<List<List<(int, int, string)>>>());
+            Assert.That(log.Events[0][0], Is.EqualTo((0, 0, "assigned")));
+            Assert.That(log.Tasks[0], Is.EqualTo((0, 21, 6)));
+
+            log = await logFileDataAccess.Load("../../../../MekkdonaldsWPF/samples/warehouse_100_log.json");
+            Assert.That(log.Errors[0], Is.EqualTo((-1, -1, 1, "incorrect vector size")));
+        });
+    }
+
+    [Test]
+    public async Task LogWriteTest()
+    {
+        var logFileDataAccess = new LogFileDataAccess();
+        var log = new LogFile()
+        {
+            ActionModel = "MAPF_T",
+            TeamSize = 20,
+            Start = [ (new Point(4, 6), Direction.East) ],
+            NumTaskFinished = 338,
+            SumOfCost = 10000,
+            Makespan = 500,
+            ActualPaths = [ [ Action.F ] ],
+            PlannerPaths = [ [ Action.F ] ],
+            PlannerTimes = [ 0.312150264 ],
+            Errors = [],
+            Events = [[(0, 0, "assigned")]],
+            Tasks = [ (0, 21, 6) ]
+        };
+
+        await logFileDataAccess.Save("./test_log.json", log);
+
+        log = await logFileDataAccess.Load("./test_log.json");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(log.ActionModel, Is.EqualTo("MAPF_T"));
+            Assert.That(log.TeamSize, Is.EqualTo(20));
+            Assert.That(log.Start[0], Is.EqualTo((new Point(4, 6), Direction.East)));
+            Assert.That(log.NumTaskFinished, Is.EqualTo(338));
+            Assert.That(log.SumOfCost, Is.EqualTo(10000));
+            Assert.That(log.Makespan, Is.EqualTo(500));
+            Assert.That(log.ActualPaths[0][0], Is.EqualTo(Action.F));
+            Assert.That(log.PlannerPaths[0][0], Is.EqualTo(Action.F));
+            Assert.That(log.PlannerTimes[0], Is.EqualTo(0.312150264));
+            Assert.That(log.Errors, Is.Empty);
+            Assert.That(log.Events, Is.TypeOf<List<List<(int, int, string)>>>());
+            Assert.That(log.Events[0][0], Is.EqualTo((0, 0, "assigned")));
+            Assert.That(log.Tasks[0], Is.EqualTo((0, 21, 6)));
+        });
     }
 }
