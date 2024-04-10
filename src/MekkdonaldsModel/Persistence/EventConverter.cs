7eae2206761b9f5fa21264cc1d37c1a5f85523a8
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-04-10 18:56:33 +0200

    Rewrote EventConverter

--- src/MekkdonaldsModel/Persistence/EventConverter.cs
+++ src/MekkdonaldsModel/Persistence/EventConverter.cs
@@ -7,50 +7,104 @@ namespace MekkdonaldsModel.Persistence
     {
         public override List<List<(int, int, string)>> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            var value = reader.GetString() ?? throw new NullReferenceException();
-            value = value.Replace("\n", "");
-            value = value.Replace("\t", "");
-            value = value.Replace(" ", "");
-            value = value.Replace("[[", "lb");
-            value = value.Replace("]]", "le");
-            value = value.Replace("[", "");
-            value = value.Replace("]", "");
-            var values = value.Split("lb");
-            List<string> stringl = new();
-            foreach (var item in values)
+            if (reader.TokenType != JsonTokenType.StartArray)
             {
-                if (item != "") { stringl.Add(item); }
+                throw new JsonException();
             }
-            List<List<(int, int, string)>> lista = new();
-            List<(int, int, string)> kislista = new();
-            foreach (var item in stringl)
-            {
-                values = item.Split(',');
-                kislista.Clear();
-                for (int i = 0; i < values.Length; i += 3)
-                {
-                    var tuple = (int.Parse(values[i]), int.Parse(values[i + 1]), values[i + 1]);
-                    kislista.Add(tuple);
-                }
-                lista.Add(kislista);
 
+            var list = new List<List<(int, int, string)>>();
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndArray)
+                {
+                    return list;
+                }
+
+                list.Add(ReadList(ref reader));
             }
-            return lista;
+
+            throw new JsonException();
         }
 
         public override void Write(Utf8JsonWriter writer, List<List<(int, int, string)>> value, JsonSerializerOptions options)
         {
+            writer.WriteStartArray();
             foreach (var item in value)
             {
-                writer.WriteStringValue($"[");
-                foreach (var item2 in item)
+                writer.WriteStartArray();
+                foreach (var (fst, snd, th) in item)
                 {
-                    writer.WriteStringValue($"[{item2.Item1},{item2.Item2},{item2.Item3}]");
+                    writer.WriteStartArray();
+                    writer.WriteNumberValue(fst);
+                    writer.WriteNumberValue(snd);
+                    writer.WriteStringValue(th);
+                    writer.WriteEndArray();
                 }
-                writer.WriteStringValue($"]");
+                writer.WriteEndArray();
+            }
+            writer.WriteEndArray();
+        }
+
+        private static List<(int, int, string)> ReadList(ref Utf8JsonReader reader)
+        {
+            if (reader.TokenType != JsonTokenType.StartArray)
+            {
+                throw new JsonException();
             }
 
+            var list = new List<(int, int, string)>();
 
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndArray)
+                {
+                    return list;
+                }
+
+                list.Add(ReadValue(ref reader));
+            }
+
+            throw new JsonException();
+
+        }
+
+        private static (int, int, string) ReadValue(ref Utf8JsonReader reader)
+        {
+            int fst = 0, snd = 0, i = 0;
+            string str = "";
+
+            if (reader.TokenType != JsonTokenType.StartArray)
+            {
+                throw new JsonException();
+            }
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndArray)
+                {
+                    return (fst, snd, str);
+                }
+
+                if (reader.TokenType == JsonTokenType.Number)
+                {
+                    if (i++ == 0)
+                    {
+                        fst = reader.GetInt32();
+                    }
+                    else
+                    {
+                        snd = reader.GetInt32();
+                    }
+
+                }
+                else if (reader.TokenType == JsonTokenType.String)
+                {
+                    str = reader.GetString() ?? throw new JsonException();
+                }
+            }
+
+            throw new JsonException();
         }
     }
 }
