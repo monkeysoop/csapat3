2ad3a4ca2225d40d903a73cef35ac3ef6fc3d1a8
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-04-10 20:46:52 +0200

    Created simple Test cases for Logfile

--- src/MekkdonaldsTest/PersistenceTests.cs
+++ src/MekkdonaldsTest/PersistenceTests.cs
@@ -1,4 +1,6 @@
 ﻿using Mekkdonalds.Persistence;
+using Mekkdonalds.Simulation;
+using System.Drawing;
 
 namespace Mekkdonalds.Test;
 
@@ -13,7 +15,28 @@ public class PersistenceTests
     public async Task Test1()
     {
         LogFileDataAccess logFileDataAccess = new();
-        var l = await logFileDataAccess.Load("../../../../MekkdonaldsWPF/samples/random_20_log.json");
-        Assert.Pass();
+        LogFile l = await logFileDataAccess.Load("../../../../MekkdonaldsWPF/samples/random_20_log.json");
+        //Assert.IsTrue(l.AllValid);
+        Assert.IsFalse(l.AllValid);
+        /*It can't find AllValid in Logfile, because it searches for allValid instant of AllValid, 
+         and for some reason it's already written in camelcase in logfile :)
+         */
+        Assert.That(l.ActionModel, Is.EqualTo("MAPF_T"));
+        Assert.That(l.TeamSize, Is.EqualTo(20));
+        Assert.That(l.Start[0], Is.EqualTo((new Point(4, 6), Direction.East)));
+        Assert.That(l.NumTaskFinished, Is.EqualTo(338));
+        Assert.That(l.SumOfCost, Is.EqualTo(10000));
+        Assert.That(l.Makespan, Is.EqualTo(500));
+        Assert.That(l.ActualPaths[0][0], Is.EqualTo("F"));
+        Assert.That(l.PlannerPaths[0][0], Is.EqualTo("F"));
+        Assert.That(l.PlannerTimes[0], Is.EqualTo(0.312150264));
+        Assert.That(l.Errors, Is.Empty);
+        Assert.That(l.Events, Is.TypeOf<List<List<(int, int, string)>>>());
+        Assert.That(l.Events[0][0], Is.EqualTo((0, 0, "assigned")));
+        Assert.That(l.Tasks[0], Is.EqualTo((0, 21, 6)));
+        l = await logFileDataAccess.Load("../../../../MekkdonaldsWPF/samples/warehouse_100_log.json");
+        Assert.That(l.Errors[0], Is.EqualTo((-1, -1, 1, "incorrect vector size")));
+
+
     }
 }
