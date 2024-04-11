becd0fb80da156c0120c14f47baff4b8fed8f7b9
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-04-11 10:59:05 +0200

    Changed Query to nullable

--- src/MekkdonaldsModel/IntervalTree.cs
+++ src/MekkdonaldsModel/IntervalTree.cs
@@ -9,9 +9,9 @@ public class IntervalTree<TValue> : IntervalTree<int, TValue>
         base.Add(from, to - 1, value);
     }
 
-    public new TValue Query(int key) => Query(key, key).FirstOrDefault()!;
+    public new TValue? Query(int key) => Query(key, key).FirstOrDefault();
 
-    public TValue this[int key] => Query(key);
+    public TValue? this[int key] => Query(key);
 
     public TValue this[int from, int to]
     {
