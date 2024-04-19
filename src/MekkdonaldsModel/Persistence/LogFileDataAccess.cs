f3c20338950f091652d85336ecd45bc9f08be8ea
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-04-19 15:28:51 +0200

    Made LogFile save prettier :)

--- src/MekkdonaldsModel/Persistence/LogFileDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/LogFileDataAccess.cs
@@ -5,7 +5,7 @@ namespace Mekkdonalds.Persistence;
 
 public class LogFileDataAccess : ILogFileDataAccess
 {
-    public static readonly JsonSerializerOptions SerializerOptions = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, PropertyNameCaseInsensitive = true };
+    public static readonly JsonSerializerOptions SerializerOptions = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, PropertyNameCaseInsensitive = true, WriteIndented = true };
 
     public async Task<LogFile> LoadAsync(string path) => JsonSerializer.Deserialize<LogFile>(await File.ReadAllTextAsync(path), SerializerOptions) ?? throw new LogFileDataException();
 
