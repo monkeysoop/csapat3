40b4dce4e135f9aab4e1623b12d430a82157cc68
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-04-22 11:44:22 +0200

    Logger test init

---
+++ src/MekkdonaldsTest/LoggerTests.cs
@@ -0,0 +1,31 @@
+﻿using Mekkdonalds.Simulation;
+using NUnit.Framework.Legacy;
+using System.Drawing;
+
+namespace MekkdonaldsTest;
+
+internal class LoggerTests
+{
+    private Logger _logger;
+
+    [SetUp]
+    public void Setup()
+    {
+        _logger = new Logger("test");
+    }
+
+    [Test]
+    public void DirectoryCreateTest()
+    {
+        Assert.That(Directory.Exists("./logs"));
+    }
+
+    [Test]
+    public void StartLogTests()
+    {
+        var rs = new List<Robot>();
+        rs.AddRange(Enumerable.Range(0, 10).Select(_ => new Robot(new(Random.Shared.Next(30), Random.Shared.Next(30)), (Direction)Random.Shared.Next(4))));
+        _logger.LogStarts(rs);
+        Assert.That(rs, Is.EqualTo(_logger.GetLogFile().Start).AsCollection);
+    }
+}
