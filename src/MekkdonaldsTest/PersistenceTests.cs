4fe29cb506929163c907eaf28c3265ded2605c40
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-04-22 11:06:26 +0200

    Updated TestBoardLoading to use Getter Method

--- src/MekkdonaldsTest/PersistenceTests.cs
+++ src/MekkdonaldsTest/PersistenceTests.cs
@@ -66,9 +66,12 @@ public class PersistenceTests
         Assert.That(board is not null);
         Assert.That(board!.Height, Is.EqualTo(34));
         Assert.That(board!.Width, Is.EqualTo(34));
-        Assert.That(board.Data[0], Is.EqualTo(1));
-        Assert.That(board.Data[^1], Is.EqualTo(1));
-        Assert.That(board.Data[13], Is.EqualTo(1));
+        Assert.That(board.GetValue(0, 0), Is.EqualTo(1));
+        Assert.That(board.GetValue(1, 1), Is.EqualTo(0));
+        Assert.That(board.GetValue(board.Height - 1, board.Width - 1), Is.EqualTo(1));
+        Assert.That(board.GetValue(3, 3), Is.EqualTo(0));
+        Assert.That(board.GetValue(14, 13), Is.EqualTo(1));
+
 
     }
 
