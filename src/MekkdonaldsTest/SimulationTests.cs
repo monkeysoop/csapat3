d1bc211f8efeca7affa409c397fd3e8339334bd2
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-04-22 11:28:43 +0200

    Created test blocks inside TestPath

--- src/MekkdonaldsTest/SimulationTests.cs
+++ src/MekkdonaldsTest/SimulationTests.cs
@@ -10,6 +10,8 @@ namespace Mekkdonalds.Test;
 
 public class SimulationTests
 {
+    private Path? path;
+
     [SetUp]
     public void Setup()
     {
@@ -19,50 +21,62 @@ public class SimulationTests
 
     public void TestPath()
     {
-        Path path = new(new List<Action>(), new Point(0, 0));
-        Assert.That(path.IsOver, Is.True);
-        Assert.That(path[0] is null);
-        Assert.That(path[-3] is null);
-        Assert.That(path[13] is null);
+        path = new(new List<Action>(), new Point(0, 0));
+        Assert.Multiple(() =>
+        {
+            Assert.That(path.IsOver, Is.True);
+            Assert.That(path[0] is null);
+            Assert.That(path[-3] is null);
+            Assert.That(path[13] is null);
+        });
         Assert.Throws<System.Exception>(() => path.Next());
         path = new(new List<Action>() { Action.F }, new Point(0, 0));
-        Assert.That(path.IsOver, Is.False);
-        Assert.That(path[0], Is.EqualTo(Action.F));
-        Assert.That(path[-3] is null);
-        Assert.That(path[13] is null);
-        Assert.That(path.Next(), Is.EqualTo(Action.F));
-        Assert.That(path.IsOver, Is.True);
+        Assert.Multiple(() =>
+        {
+            Assert.That(path.IsOver, Is.False);
+            Assert.That(path[0], Is.EqualTo(Action.F));
+            Assert.That(path[-3] is null);
+            Assert.That(path[13] is null);
+            Assert.That(path.Next(), Is.EqualTo(Action.F));
+            Assert.That(path.IsOver, Is.True);
+        });
         Assert.Throws<System.Exception>(() => path.Next());
         path = new(new List<Action>() { Action.F, Action.C }, new Point(0, 0));
-        Assert.That(path.IsOver, Is.False);
-        Assert.That(path[0], Is.EqualTo(Action.F));
-        Assert.That(path[1], Is.EqualTo(Action.C));
-        Assert.That(path[-3] is null);
-        Assert.That(path[13] is null);
-        Assert.That(path.Next(), Is.EqualTo(Action.F));
-        Assert.That(path.IsOver, Is.False);
-        Assert.That(path.Next(), Is.EqualTo(Action.C));
-        Assert.That(path.IsOver, Is.True);
+        Assert.Multiple(() =>
+        {
+            Assert.That(path.IsOver, Is.False);
+            Assert.That(path[0], Is.EqualTo(Action.F));
+            Assert.That(path[1], Is.EqualTo(Action.C));
+            Assert.That(path[-3] is null);
+            Assert.That(path[13] is null);
+            Assert.That(path.Next(), Is.EqualTo(Action.F));
+            Assert.That(path.IsOver, Is.False);
+            Assert.That(path.Next(), Is.EqualTo(Action.C));
+            Assert.That(path.IsOver, Is.True);
+        });
         Assert.Throws<System.Exception>(() => path.Next());
         path = new(new List<Action>() { Action.F, Action.C, Action.R, Action.T, Action.B, Action.F }, new Point(0, 0));
-        Assert.That(path.IsOver, Is.False);
-        Assert.That(path[0], Is.EqualTo(Action.F));
-        Assert.That(path[1], Is.EqualTo(Action.C));
-        Assert.That(path[5], Is.EqualTo(Action.F));
-        Assert.That(path[-3] is null);
-        Assert.That(path[13] is null);
-        Assert.That(path.Next(), Is.EqualTo(Action.F));
-        Assert.That(path.IsOver, Is.False);
-        Assert.That(path.Next(), Is.EqualTo(Action.C));
-        Assert.That(path.IsOver, Is.False);
-        Assert.That(path.Next(), Is.EqualTo(Action.R));
-        Assert.That(path.IsOver, Is.False);
-        Assert.That(path.Next(), Is.EqualTo(Action.T));
-        Assert.That(path.IsOver, Is.False);
-        Assert.That(path.Next(), Is.EqualTo(Action.B));
-        Assert.That(path.IsOver, Is.False);
-        Assert.That(path.Next(), Is.EqualTo(Action.F));
-        Assert.That(path.IsOver, Is.True);
+        Assert.Multiple(() =>
+        {
+            Assert.That(path.IsOver, Is.False);
+            Assert.That(path[0], Is.EqualTo(Action.F));
+            Assert.That(path[1], Is.EqualTo(Action.C));
+            Assert.That(path[5], Is.EqualTo(Action.F));
+            Assert.That(path[-3] is null);
+            Assert.That(path[13] is null);
+            Assert.That(path.Next(), Is.EqualTo(Action.F));
+            Assert.That(path.IsOver, Is.False);
+            Assert.That(path.Next(), Is.EqualTo(Action.C));
+            Assert.That(path.IsOver, Is.False);
+            Assert.That(path.Next(), Is.EqualTo(Action.R));
+            Assert.That(path.IsOver, Is.False);
+            Assert.That(path.Next(), Is.EqualTo(Action.T));
+            Assert.That(path.IsOver, Is.False);
+            Assert.That(path.Next(), Is.EqualTo(Action.B));
+            Assert.That(path.IsOver, Is.False);
+            Assert.That(path.Next(), Is.EqualTo(Action.F));
+            Assert.That(path.IsOver, Is.True);
+        });
         Assert.Throws<System.Exception>(() => path.Next());
     }
 }
