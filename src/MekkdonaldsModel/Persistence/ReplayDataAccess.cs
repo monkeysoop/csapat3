63e37a462ede62b06a9a90f50fef47e08c9b4cd5
Nagy-Mohos Adorján Bence <d5vd5e@inf.elte.hu>
2024-04-27 14:16:33 +0200

    Added singelton instance

--- src/MekkdonaldsModel/Persistence/ReplayDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/ReplayDataAccess.cs
@@ -5,4 +5,18 @@ public class ReplayDataAccess : IReplayDataAccess
     public required IBoardDataAccess BDA { get; init; }
 
     public required ILogFileDataAccess LDA { get; init; }
+
+    private static ReplayDataAccess? _instance;
+
+    public static ReplayDataAccess Instance
+    {
+        get
+        {
+            return _instance ??= new()
+            {
+                BDA = new BoardFileDataAccess(),
+                LDA = new LogFileDataAccess()
+            };
+        }
+    }
 }
