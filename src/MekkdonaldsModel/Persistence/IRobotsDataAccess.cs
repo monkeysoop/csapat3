711622ef3a2f153da30195fdf0d8118ced0ca553
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-09 20:15:12 +0200

    Created documentation for IRobotsDataAccess

--- src/MekkdonaldsModel/Persistence/IRobotsDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/IRobotsDataAccess.cs
@@ -2,6 +2,13 @@
 
 public interface IRobotsDataAccess
 {
+    /// <summary>
+    /// Load method to override
+    /// </summary>
+    /// <param name="path">path to the file</param> 
+    /// <param name="width">width of the board</param> 
+    /// <param name="height">height of the board</param> 
+    /// <returns>task that represents the loading operation. The task result contains the list of robots</returns> 
     internal Task<List<Robot>> LoadAsync(string path, int width, int height);
     // internal Task SaveAsync(string path, List<Robot> packages); - do we need this?
 }
