cc06ef865fa8b49d90e75ccb9527dd1d1b134dab
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-10 00:45:14 +0200

    Created documentation for ReplayDataAccess class

--- src/MekkdonaldsModel/Persistence/ReplayDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/ReplayDataAccess.cs
@@ -2,12 +2,20 @@
 
 public class ReplayDataAccess : IReplayDataAccess
 {
+    /// <summary>
+    /// data access for the board
+    /// </summary>
     public required IBoardDataAccess BDA { get; init; }
-
+    /// <summary>
+    /// data access for the log files
+    /// </summary>
     public required ILogFileDataAccess LDA { get; init; }
 
     private static ReplayDataAccess? _instance;
 
+    /// <summary>
+    /// creates a new instance of the ReplayDataAccess class
+    /// </summary>
     public static ReplayDataAccess Instance
     {
         get
