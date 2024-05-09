8c6780e2033918f9361f0195d5f780b6b5dca939
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-10 00:44:51 +0200

    Created documentation for LogFileDataAccess class

--- src/MekkdonaldsModel/Persistence/LogFileDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/LogFileDataAccess.cs
@@ -4,9 +4,22 @@ namespace Mekkdonalds.Persistence;
 
 public class LogFileDataAccess : ILogFileDataAccess
 {
+    /// <summary>
+    /// naming policy for the JSON serializer
+    /// </summary>
     public static readonly JsonSerializerOptions SerializerOptions = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, PropertyNameCaseInsensitive = true, WriteIndented = true };
-
+    /// <summary>
+    /// loads a log file from the given path
+    /// </summary>
+    /// <param name="path">path to the file</param> 
+    /// <returns>A task that represents the Loading operation. The task result contains the log file</returns> 
+    /// <exception cref="LogFileDataException">thrown when the data is invalid</exception> 
     public async Task<LogFile> LoadAsync(string path) => JsonSerializer.Deserialize<LogFile>(await File.ReadAllTextAsync(path), SerializerOptions) ?? throw new LogFileDataException();
-
+    /// <summary>
+    /// saves a log file to the given path
+    /// </summary>
+    /// <param name="path"> path to the file</param>
+    /// <param name="logFile"> log file to save</param>
+    /// <returns>A task that represents the save operation</returns> 
     public async Task SaveAsync(string path, LogFile logFile) => await File.WriteAllTextAsync(path, JsonSerializer.Serialize(logFile, SerializerOptions));
 }
