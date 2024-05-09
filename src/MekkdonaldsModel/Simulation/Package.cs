cfc0726e3724fcfe1b260c9d58abc4abf57a2c34
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-10 00:46:22 +0200

    Created documentation for Package class

--- src/MekkdonaldsModel/Simulation/Package.cs
+++ src/MekkdonaldsModel/Simulation/Package.cs
@@ -4,12 +4,26 @@ public sealed class Package(Point p)
 {
     private static int IDCounter = 1;
 
+    /// <summary>
+    /// id of the package
+    /// </summary>
     public int ID { get; } = IDCounter++;
 
+    /// <summary>
+    /// Position of the package
+    /// </summary>
     public Point Position { get; } = p;
 
+    /// <summary>
+    /// package constructor
+    /// </summary>
+    /// <param name="x">x coordinate of the package</param>  
+    /// <param name="y">y coordinate of the package</param> 
     public Package(int x, int y) : this(new Point(x, y)) { }
 
+    /// <summary>
+    /// resets the ID counter to 1
+    /// </summary>
     public static void ResetIDCounter()
     {
         IDCounter = 1;
