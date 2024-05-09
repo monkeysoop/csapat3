8618495b3477a2e30fefc06d8a8455777f6f97f1
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-09 20:14:51 +0200

    Created documentation for IPackagesDataAccess interface

--- src/MekkdonaldsModel/Persistence/IPackagesDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/IPackagesDataAccess.cs
@@ -2,6 +2,13 @@
 
 public interface IPackagesDataAccess
 {
+    /// <summary>
+    /// Load method to override
+    /// </summary>
+    /// <param name="path">path to the file</param> 
+    /// <param name="width">width of the board</param> 
+    /// <param name="height">height of the board</param> 
+    /// <returns>task that represents the loading operation. The task result contains the list of packages</returns> 
     internal Task<List<Package>> LoadAsync(string path, int width, int height);
     // internal Task SaveAsync(string path, List<Package> packages); // not needed for tasks
 }
