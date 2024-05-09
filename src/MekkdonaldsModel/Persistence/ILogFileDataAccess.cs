1d9fd29c3925765dddcae1cec321cecea3a5ccf2
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-09 20:14:38 +0200

    Created documentation for ILogFileDataAccess interface

--- src/MekkdonaldsModel/Persistence/ILogFileDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/ILogFileDataAccess.cs
@@ -2,6 +2,17 @@
 
 public interface ILogFileDataAccess
 {
+    /// <summary>
+    /// Load method for the log files
+    /// </summary>
+    /// <param name="path"> path to the file</param>
+    /// <returns>task that represents the loading operation. The task result contains the log file</returns> 
     Task<LogFile> LoadAsync(string path);
+    /// <summary>
+    /// save method for the log files
+    /// </summary>
+    /// <param name="path">path to the file</param> 
+    /// <param name="logFile">log file to save</param> 
+    /// <returns>task that represents the saving operation</returns> 
     Task SaveAsync(string path, LogFile logFile);
 }
