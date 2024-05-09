4d82d279c342feb8663e53b819a1309ac4dba9e4
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-10 00:44:33 +0200

    Created documentation for ISimDataAccess interface

--- src/MekkdonaldsModel/Persistence/ISimDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/ISimDataAccess.cs
@@ -2,9 +2,24 @@
 
 public interface ISimDataAccess
 {
+    /// <summary>
+    /// data access for the config
+    /// </summary>
     public IConfigDataAccess CDA { get; init; }
+    /// <summary>
+    /// data access for the board
+    /// </summary>
     public IBoardDataAccess BDA { get; init; }
+    /// <summary>
+    /// data access for the packages
+    /// </summary>
     public IPackagesDataAccess PDA { get; init; }
+    /// <summary>
+    /// data access for the robots
+    /// </summary>
     public IRobotsDataAccess RDA { get; init; }
+    /// <summary>
+    /// data access for the log files
+    /// </summary>
     public ILogFileDataAccess LDA { get; init; }
 }
