3d44c504349774e2c9423e544d6fff99464ef9fa
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-09 20:13:47 +0200

    Created documentation for Config class

--- src/MekkdonaldsModel/Persistence/Config.cs
+++ src/MekkdonaldsModel/Persistence/Config.cs
@@ -6,12 +6,29 @@ namespace Mekkdonalds.Persistence;
 
 public class Config
 {
+    /// <summary>
+    /// Map file path
+    /// </summary>
     public required string MapFile { get; set; }
+    /// <summary>
+    /// Agent file path
+    /// </summary>
     public required string AgentFile { get; set; }
+    /// <summary>
+    /// Task file path
+    /// </summary>
     public required string TaskFile { get; set; }
-
+    /// <summary>
+    /// number of robots
+    /// </summary>
     public int TeamSize { get; set; }
+    /// <summary>
+    /// Number of tasks to reveal
+    /// </summary>
     public int NumTasksReveal { get; set; }
+    /// <summary>
+    /// strategy for task assignment
+    /// </summary>
 
     [JsonConverter(typeof(StrategyConverter))]
     public Strategy TaskAssignmentStrategy { get; set; }
