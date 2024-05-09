57f1cc640229b3c063ca43202d43c63e4a615aef
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-09 20:14:24 +0200

    Created documentation for IConfigDataAccess interface

--- src/MekkdonaldsModel/Persistence/IConfigDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/IConfigDataAccess.cs
@@ -2,5 +2,10 @@
 
 public interface IConfigDataAccess
 {
+    /// <summary>
+    /// Load method for the config
+    /// </summary>
+    /// <param name="path">path to the file</param> 
+    /// <returns>task that represents the loading operation. The task result contains the config</returns> 
     public Task<Config> LoadAsync(string path);
 }
