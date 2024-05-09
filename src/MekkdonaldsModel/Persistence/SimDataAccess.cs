3f98ed4b20273690e3ef11fac16e648d2726c3a0
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-10 00:45:41 +0200

    Created documentation for SimDataAccess class

--- src/MekkdonaldsModel/Persistence/SimDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/SimDataAccess.cs
@@ -2,14 +2,33 @@
 
 public class SimDataAccess : ISimDataAccess
 {
+
+    /// <summary>
+    /// data access object for the configuration
+    /// </summary>
     public required IConfigDataAccess CDA { get; init; }
+    /// <summary>
+    /// data access object for the board
+    /// </summary>
     public required IBoardDataAccess BDA { get; init; }
+    /// <summary>
+    /// data access object for the packages
+    /// </summary>
     public required IPackagesDataAccess PDA { get; init; }
+    /// <summary>
+    /// data access object for the robots
+    /// </summary>
     public required IRobotsDataAccess RDA { get; init; }
+    /// <summary>
+    /// data access object for the log files
+    /// </summary>
     public required ILogFileDataAccess LDA { get; init; }
 
     private static SimDataAccess? _instance;
 
+    /// <summary>
+    /// creates a new instance of the SimDataAccess class
+    /// </summary>
     public static SimDataAccess Instance => _instance ??= new()
     {
         CDA = new ConfigDataAccess(),
