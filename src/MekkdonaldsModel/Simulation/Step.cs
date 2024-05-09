e8e2d3a5e964ffd8f5a0826bdcb207f1b4cf66ae
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-10 00:46:39 +0200

    Created documentation for Step class

--- src/MekkdonaldsModel/Simulation/Step.cs
+++ src/MekkdonaldsModel/Simulation/Step.cs
@@ -2,7 +2,16 @@
 
 public readonly struct Step(Point position, int direction, int heuristic)
 {
+    /// <summary>
+    /// position of the robot
+    /// </summary>
     public Point Position { get; init; } = position;
+    /// <summary>
+    /// direction the robot is facing
+    /// </summary>
     public int Direction { get; init; } = direction;
+    /// <summary>
+    /// heuristic value of the step
+    /// </summary>
     public int Heuristic { get; init; } = heuristic;
 }
