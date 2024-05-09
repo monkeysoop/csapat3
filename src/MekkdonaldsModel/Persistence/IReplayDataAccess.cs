52ddc4ac4401f6f609bdbe9fc55ad6b94014fe6f
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-09 20:15:03 +0200

    Created documentation for IReplayDataAccess interface

--- src/MekkdonaldsModel/Persistence/IReplayDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/IReplayDataAccess.cs
@@ -2,6 +2,12 @@
 
 public interface IReplayDataAccess
 {
+    /// <summary>
+    /// data access for the board
+    /// </summary>
     public IBoardDataAccess BDA { get; init; }
+    /// <summary>
+    /// data access for the log files
+    /// </summary>
     public ILogFileDataAccess LDA { get; init; }
 }
