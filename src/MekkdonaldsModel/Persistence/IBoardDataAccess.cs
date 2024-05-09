a6323dd4c15409978aeffa9548bff7104586d005
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-09 20:14:11 +0200

    Created documentation for IBoardDataAccess class

--- src/MekkdonaldsModel/Persistence/IBoardDataAccess.cs
+++ src/MekkdonaldsModel/Persistence/IBoardDataAccess.cs
@@ -2,6 +2,17 @@
 
 public interface IBoardDataAccess
 {
+    /// <summary>
+    /// Load a board from a file
+    /// </summary>
+    /// <param name="path">path to the file</param> 
+    /// <returns>task that represents the loading operation. The task result contains the board</returns> 
     internal Task<Board> LoadAsync(string path);
+    /// <summary>
+    /// Save a board to a file
+    /// </summary>
+    /// <param name="path"> path to the file</param>
+    /// <param name="board">board to save</param> 
+    /// <returns>task that represents the saving operation</returns> 
     internal Task SaveAsync(string path, Board board);
 }
