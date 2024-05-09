034ad6f03956acf73a9570283bccfa62b1c89769
Baranyi Sándor <ct9xfj@inf.elte.hu>
2024-05-10 00:46:48 +0200

    Created documentation for Wall class

--- src/MekkdonaldsModel/Simulation/Wall.cs
+++ src/MekkdonaldsModel/Simulation/Wall.cs
@@ -2,7 +2,14 @@
 
 public sealed class Wall(Point x)
 {
+    /// <summary>
+    /// position of the wall
+    /// </summary>
     public Point Position { get; } = x;
-
+    /// <summary>
+    /// constructor for the wall
+    /// </summary>
+    /// <param name="x">x coordinate of the wall</param> 
+    /// <param name="y">y coordinate of the wall</param> 
     public Wall(int x, int y) : this(new(x, y)) { }
 }
